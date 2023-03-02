2560e1ecd60c3fd6ba6b2cf4d17af7e179dbfbf8
SarthakGunjal <sarthakgunjal12345@gmail.com>
2023-03-02 08:56:33 +0530

    adding v2.0.0 for the project

--- app.js
+++ app.js
@@ -12,11 +12,20 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-app.get("/" , function(req , res){
-    res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  console.log('v1');
+  res.sendFile(__dirname + "/v1.html");
+})
+
+// app.get("/v1", function (req, res) {
+//   res.sendFile(__dirname + "/v1.html");
+// })
+
+app.get("/v2", function (req, res) {
+  res.sendFile(__dirname + "/v2.html");
 })
 //TODO
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
