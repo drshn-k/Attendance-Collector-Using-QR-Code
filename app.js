e660a44843145f86651aa71c0e040c7d5fde3070
drshn-k <dkasar258@gmail.com>
2023-04-11 09:25:22 +0530

    xlsx yet to resolve, V2.0.0  accomplished

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+// import {Html5QrcodeScanner} from "html5-qrcode"
 
+// To use Html5Qrcode (more info below)
+// import { Html5QrcodeScanner } from "https://unpkg.com/html5-qrcode?module";
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,6 +20,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/v1.html");
 })
 
+app.post("/compose" , function(req, res){
+  console.log(req.body.paragraph);
+  res.redirect("/");
+})
 // app.get("/v1", function (req, res) {
 //   res.sendFile(__dirname + "/v1.html");
 // })
@@ -29,3 +36,28 @@ app.get("/v2", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+// const scanner = new Html5QrcodeScanner('reader', {
+//   qrbox: {
+//     width: 250,
+//     height: 250,
+//   },
+//   fps: 20,
+// });
+// scanner.render(success, error);
+
+// function success(result) {
+//   console.log(result);
+//   var s = typeof result;
+//   console.log(s);
+//   document.getElementById('result').innerHTML = `
+//     <h2>Success!</h2>
+//     <p><a href="${result}">${result}</a></p>
+//   `;
+//   scanner.clear();
+//   document.getElementById('reader').remove();
+// }
+
+function error(err) {
+  console.error(err);
+}
